e95f5fc53302828190c5fa235e918b70434d34a6
gtnh48965 <gtnh48965@bk.ru>
2020-08-12 00:48:43 +0300

    закночли адаптивность и первый слайд 12

--- Flask/app.py
+++ Flask/app.py
@@ -5,8 +5,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    return render_template('body.html')
+
+@app.route('/documents/1')
+def body2():
     return render_template('body2.html')
+
+@app.route('/documents/2')
+def body2_2():
+    return render_template('body2_2.html')
+
+@app.route('/documents/3')
+def body2_3():
+    return render_template('body2_3.html')
+
+@app.route('/documents/4')
+def body2_4():
+    return render_template('body2_4.html')
+@app.route('/documents/5')
+def body2_5():
+    return render_template('body2_5.html')
+
+@app.route('/documents/6')
+def body2_6():
+    return render_template('body2_6.html')
 
 
 if __name__ == '__main__':
     app.run()
+
